ef7bb83cd6bb5415cba23c67db1c33906a9e3ff8
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-06 14:09:04 -0500

    Location seeding ID into User while being created

--- my-app/server/models/Location.js
+++ my-app/server/models/Location.js
@@ -21,7 +21,7 @@ const locationSchema = new Schema({
   // Note from Tim: Only using to hold a list of user names. Since all user names are unique, we can use this array to match specific user name as we don't need any other user info at this time.
   members: [
     {
-      memberName: {
+      member: {
         type: String,
         required: true,
         trim: true,
--- my-app/server/seeds/seed.js
+++ my-app/server/seeds/seed.js
@@ -2,26 +2,38 @@ const db = require('../config/connection');
 const { User, Location } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const locationSeeds = require('./locationSeeds.json');
+
+// Resets DB and seeds
 db.once('open', async () => {
   try {
-    // Clears out DB
+    // Clears out DB collections
     await User.deleteMany({});
+    await Location.deleteMany({});
 
-    // Creates new collections
-    // Creates users
+    // Creates new collections for each module
     await User.create(userSeeds);
 
-    // Adds location along with the user
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
+
+    // Adds location to user documents karmaGroups [] / Links Location to Users
+    // Loops through locationSeeds []
+    for (let i = 0; i < locationSeeds.length; i++) {
+      const { _id } = await Location.create(locationSeeds[i]);
+console.log('This happened', locationSeeds[i].members);
+      // Loops through the members array
+      for(let k = 0; k < locationSeeds[i].members.length; k++) {
+        // sets variables 
+        let member = locationSeeds[i].members[k].member;
+
+        // Adds location data to user array karmaGroups []
+        const user = await User.findOneAndUpdate(
+          { username: member },
+          {
+            $addToSet: {
+              karmaGroups: _id,
+            },
+          }
+        );
+      }
     }
   } catch (err) {
     console.error(err);
