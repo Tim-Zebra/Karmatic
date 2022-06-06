03d5c059ed7a272e75edfab34ee5a9f0cef55578
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-06-06 10:57:50 -0500

    folder structure updated

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -20,18 +18,6 @@ function App() {
             <Route 
               path="/" 
               element={<Home />}
-            />
-            <Route 
-              path="/matchup" 
-              element={<Matchup />}
-            />
-            <Route 
-              path="/matchup/:id" 
-              element={<Vote />}
-            />
-            <Route 
-              path="*"
-              element={<NotFound />}
             />
           </Routes>
         </div>
