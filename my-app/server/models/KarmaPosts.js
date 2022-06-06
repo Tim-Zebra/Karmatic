9e6635a08a6dc50962dd2f111310c13b18e289a7
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-06 10:35:24 -0500

    added guess code to models

--- my-app/server/models/KarmaPosts.js
+++ my-app/server/models/KarmaPosts.js
@@ -1,9 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const karmaPostsSchema = new Schema({
+const karmaPostSchema = new Schema({
+  karmaPost: {
+    type: String,
+    required: 'You need to leave a thought!',
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  karmaAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  karmaHelpers: [
+    {
+      helperUsername: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
 
 });
 
-const KarmaPosts = model('KarmaPosts', karmaPostsSchema);
+const KarmaPost = model('KarmaPosts', karmaPostSchema);
 
-module.exports = KarmaPosts;
+module.exports = KarmaPost;
