723aa1b0585c1d85b7e8c8a31ce66801eb25c4b1
John Yates <johndyates@gmail.com>
2022-06-08 10:57:24 -0500

    updated login/signup form

--- my-app/client/src/components/Forms/SignupForm.js
+++ my-app/client/src/components/Forms/SignupForm.js
@@ -3,18 +3,18 @@ import { StyledForm } from './Form.Styled'
 import SubmitButton from '../Buttons/SubmitButton';
 import ToggleButton from '../Buttons/ToggleButton';
 
-export default function LoginForm({ handleSubmit, handleLoginSignupToggle }) {
+export default function SignupForm({ handleSubmit, handleLoginSignupToggle }) {
     // Button text content
     const submitButtonTextContent = 'Create Account';
     const toggleButtonTextContent = 'Back to Login';
 
     return (
         <StyledForm>
+            <label for="username">username</label>
+            <input type="text" name="username" placeholder="Enter your username..." />
+
             <label for="email">email</label>
             <input type="text" name="email" placeholder="Enter your email..." />
-
-            <label for="email">confirm email</label>
-            <input type="text" name="email" placeholder="Confirm your email..." />
 
             <label for="password">password</label>
             <input type="password" name="password" placeholder="Type in your password..." />
