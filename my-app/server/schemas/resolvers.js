c2a3aebd1598e804259215fd4165cb59d312a4a5
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-08 10:38:53 -0500

    added 'populate' data to ME query

--- my-app/server/schemas/resolvers.js
+++ my-app/server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if(context.user) {
-        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        return User.findOne({ _id: context.user._id }).populate('karmaPosts').populate('karmaHelping').populate('karmaGroups');
       }
       throw new AuthenticationError('You are not logged in!');
     },
