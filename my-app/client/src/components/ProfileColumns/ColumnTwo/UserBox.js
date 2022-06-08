301559eded9eb27105c26e632ef19746a916ff3f
John Yates <johndyates@gmail.com>
2022-06-08 14:36:28 -0500

    queries and mutations made

--- my-app/client/src/components/ProfileColumns/ColumnTwo/UserBox.js
+++ my-app/client/src/components/ProfileColumns/ColumnTwo/UserBox.js
@@ -5,10 +5,10 @@ import { Container } from "./ProfileColumnTwo.styled";
 import { AddButtonWhite } from "../../Buttons/AddButton.styled";
 
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from "../../../utils/queries"
+import { GET_USER } from "../../../utils/queries"
 
 export default function UserBox() {
-    const { data } = useQuery(QUERY_USER);
+    const { data } = useQuery(GET_USER);
     const user = data?.users[0].username
 
     return (
--- my-app/client/src/utils/queries.js
+++ my-app/client/src/utils/queries.js
@@ -1,42 +1,145 @@
 import { gql } from '@apollo/client';
 
 // Gets logged in user info
+// This pulls everything excluding password
 export const GET_ME = gql`
-query me {
-  me {
+query GET_ME {
+  users {
     _id
     username
     email
-    bookCount
-    savedBooks {
-      authors
-      title
-      description
-      bookId
-      image
-      link
-      title
+    karma
+    karmaPosts {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaHelping {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaGroups {
+      _id
+      locationName
+      locationGeoTag
+      members {
+        member
+      }
     }
   }
 }
 `;
 
-// Queryies a specific user **************NOT DONE**********************
-export const QUERY_USER = gql`
-query User {
-  user {
+// Queryies a specific user -- gets everything but email and password
+export const GET_USER = gql`
+query GET_USER($username: String!) {
+  user(username: $username) {
     _id
     username
+    karma
+    karmaPosts {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaHelping {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaGroups {
+      _id
+      locationName
+      locationGeoTag
+      members {
+        member
+      }
+    }
   }
 }
 `;
 
-// Queries all users **************NOT DONE**********************
-export const QUERY_USERS = gql`
-query Users {
+// Queries all users except password
+export const GET_USERS = gql`
+query GET_USERS {
   users {
     _id
     username
+    email
+    karma
+    karmaPosts {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaHelping {
+      _id
+      postTitle
+      postDescription
+      postValue
+      duration
+      difficulty
+      createdAt
+      karmaHelpers {
+        _id
+        helperUsername
+        createdAt
+      }
+    }
+    karmaGroups {
+      _id
+      locationName
+      locationGeoTag
+      members {
+        member
+      }
+    }
   }
 }
 `;
