9c861611e963aad67b71086298b00c5f219deea5
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-11 15:53:38 -0500

    reset karma recent to 4 hours.

--- my-app/client/src/components/DashboardColumns/ColumnThree.js
+++ my-app/client/src/components/DashboardColumns/ColumnThree.js
@@ -30,7 +30,7 @@ export default function ColumnThree() {
     }
 
     // hours variable sets how far in the past the dates will be filtered.
-    const hours = 1000;
+    const hours = 4;
     const pastDate = dateFormat(Date.now() - (1000 * 60 * 60 * hours));
 
     // Filters Karma posts from the up to 4 hours in the past from the current date.
