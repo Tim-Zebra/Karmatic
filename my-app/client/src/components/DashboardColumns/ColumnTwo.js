3e90b542ccd2c6b7c7e9e84779b04c0fe0014cfa
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-06-11 17:33:55 -0500

    in progress

--- my-app/client/src/components/DashboardColumns/ColumnTwo.js
+++ my-app/client/src/components/DashboardColumns/ColumnTwo.js
@@ -3,30 +3,8 @@ import Post from '../Post/Post'
 import { ColumnContainer, FeedHeaderContainer, CreatePostContainer, FeedContainer, ActivityHeader } from './ColumnTwo.styled'
 import PostForm from '../Forms/PostForm'
 
-// Allows use for both queries and mutations from our utils folder
-import { useMutation, useQuery } from '@apollo/client';
 
-// Gets Queries
-// Gets the Karma Post
-import { GET_ALL_KARMAPOSTS } from '../../utils/queries';
-
-export default function ColumnTwo({usersKarma}) {
-    // Queries users to get karma for each user, so it can be passed to Post as prop and displayed on their posts
-console.log('column2 data',usersKarma)
-    // Queries Karma Post data
-    // Sets hooks for data loading
-    const { loading, data } = useQuery(GET_ALL_KARMAPOSTS);
-
-    const karmaPosts = data?.allKarmaPosts || [];
-
-    if (!karmaPosts) {
-        return null;
-    }
-
-    // Displays differently during loading
-    if (loading) {
-        return <h2>LOADING...</h2>;
-    }
+export default function ColumnTwo({usersKarma, karmaPosts}) {
 
     return (
         <ColumnContainer>
--- my-app/client/src/pages/Dashboard/Dashboard.js
+++ my-app/client/src/pages/Dashboard/Dashboard.js
@@ -5,19 +5,31 @@ import ColumnThree from '../../components/DashboardColumns/ColumnThree'
 
 import { DashboardContainer, Column } from './Dashboard.styled'
 import { useQuery } from '@apollo/client';
-import { GET_USERS_KARMA } from '../../utils/queries';
+import { GET_USERS_KARMA, GET_ALL_KARMAPOSTS } from '../../utils/queries';
 
 
 export default function Dashboard({ handlePageChange }) {
     const {data, loading } = useQuery(GET_USERS_KARMA);
 
+    const karmaPosts = useQuery(GET_ALL_KARMAPOSTS);
+
     if (!data) {
         return null;
     }
-
+    // Displays differently during loading
     if (loading) {
         return <h2>LOADING...</h2>
     }
+
+    if (!karmaPosts.data) {
+        return null;
+    }
+
+    // Displays differently during loading
+    if (karmaPosts.loading) {
+        return <h2>LOADING...</h2>;
+    }
+
     return (
         
         <DashboardContainer>
@@ -27,7 +39,7 @@ export default function Dashboard({ handlePageChange }) {
             </Column>
 
             <Column>
-            <ColumnTwo usersKarma={data}/>
+            <ColumnTwo usersKarma={data} karmaPosts={karmaPosts.data.allKarmaPosts}/>
             </Column>
 
             <Column>
