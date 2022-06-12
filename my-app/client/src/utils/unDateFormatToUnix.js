bb21ea5e39671d9d4b7005ad4e9a90a25546a2b3
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-12 15:32:41 -0500

    updated to milliseconds. Previous source used decimals for milliseconds...

--- my-app/client/src/utils/unDateFormatToUnix.js
+++ my-app/client/src/utils/unDateFormatToUnix.js
@@ -35,24 +35,25 @@ module.exports = (oldDate) => {
     December: 11,
   };
 
-  // Second values per minute, hour, day, month, year
-  // Multipler for time conversion. In this case converts seconds to milliseconds.
-  // Format: 1 year = 31556926 seconds
+  // Milliseconds values per minute, hour, day, month, year
+  // Format: 1 year = 31557600000 milliseconds
   const value = {
-    minute: 60,
-    hour: 3600,
-    day: 86400,
-    month : 2629743,
-    year : 31556926,
+    minute: 60000,
+    hour: 3600000,
+    day: 86400000,
+    month : 2629800000,
+    year : 31557600000,
   }
 
   // Adds based on year value Array[2]
   let year = parseInt(unixTimeStringArray[2]);
   // Added 56648 to compensate for integer rounding which results ~ 8 hours missing. Integer compensates appropriately for both 2000 and 2022
-  newUnixTime += (value.year * (year - 1970) + 56648);
+  newUnixTime += (value.year * (year - 1970));
 
   // Adds based on month value Array[0]
-
+  // let month = months[unixTimeStringArray[0]];
+  // console.log('This happened', month);
+  // newUnixTime += (value.month * month)
   // Adds based on day value Array[1]
 
   // Adds based on hours/minutes value Array[4]. Time value accounts for am and pm Array[5]. If pm then +12 hours.
