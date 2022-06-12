769880f45d913a20cdf572e60ce7e9b3e007c745
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-12 15:06:45 -0500

    added values for time. Updated names to reflect their type value

--- my-app/client/src/utils/unDateFormatToUnix.js
+++ my-app/client/src/utils/unDateFormatToUnix.js
@@ -1,8 +1,14 @@
 // Takes in the string exported by dateFormat.JS and converts it into a unix time stamp
 module.exports = (oldDate) => {
   // create month object
-  let unixTime = oldDate.split(' ');
-  console.log('unixTime after creation',unixTime);
+  // [month, day, year, 'at', time, am/pm ]
+  let unixTimeStringArray = oldDate.split(' ');
+  console.log('unixTimeArray',unixTimeStringArray);
+  
+  // newUnixTime at 0 = January 1st 1970
+  let newUnixTime = 0;
+
+
   // create month object
   const months = {
     Jan: 0,
@@ -30,6 +36,26 @@ module.exports = (oldDate) => {
     December: 11,
   };
 
-  
-  return unixTime;
+  // Millisecond values per minute, hour, day, month, year
+  // Multipler for time conversion. In this case converts seconds to milliseconds.
+  let timeMultiplier = 1000
+  // Format: 1 year = 31556926 seconds * 1000 milliseconds/seconds
+  const values = {
+    minute: 60 * timeMultiplier,
+    hour: 3600 * timeMultiplier,
+    day: 86400 * timeMultiplier,
+    month : 2629743 * timeMultiplier,
+    year : 31556926 * timeMultiplier,
+  }
+
+  // Adds based on year value Array[2]
+  let year = unixTimeStringArray
+  newUnixTime += 
+  // Adds based on month value
+
+  // Adds based on day value
+
+  // Adds based on time value. Time value accounts for am and pm. If pm then +12 hours.
+
+  return newUnixTime;
 };
