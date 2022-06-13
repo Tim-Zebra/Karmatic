268b3046510005389919283ae2f7991b199a6ac2
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-06-12 19:31:22 -0500

    profile post delete function removed, post footer text updated to easy, medium, hard

--- client/src/components/ProfileActivityCard/ProfileActivityCard.js
+++ client/src/components/ProfileActivityCard/ProfileActivityCard.js
@@ -1,36 +1,36 @@
 import React from "react";
-import { CardContainer, DeleteImage, LogoImage } from "./ProfileActivityCard.styled";
-import ProfileHelpingCard from "./ProfileHelping";
+import { CardContainer, LogoImage } from "./ProfileActivityCard.styled";
+// import ProfileHelpingCard from "./ProfileHelping";
 // Allows use for both queries and mutations from our utils folder
-import { useQuery, useMutation } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 // Gets Queries
 import { GET_ME } from '../../utils/queries';
-import { DELETE_POST, CHANGE_KARMA } from "../../utils/mutations";
+// import { DELETE_POST, CHANGE_KARMA } from "../../utils/mutations";
 
 export default function ProfileActivityCard() {
-    const [deletePost, { error, post }] = useMutation(DELETE_POST);
+    // const [deletePost, { error, post }] = useMutation(DELETE_POST);
     const { loading, data } = useQuery(GET_ME);
     const userData = data?.me || {};
-    const [refundKarma] = useMutation(CHANGE_KARMA);
+    // const [refundKarma] = useMutation(CHANGE_KARMA);
 
-    const deleteKarmaPost = async (userData, karmaPosts) => {
-        try {
-            console.log(userData)
-            await deletePost({
-                variables: { karmaPostId: karmaPosts._id, },
-            });
-            const refundedUserKarma = userData.karma + karmaPosts.postValue;
-            await refundKarma({
-                variables: {
-                    username: userData.username,
-                    karma: refundedUserKarma,
-                }
-            })
-        } catch (err) {
-            console.error(err);
-        }
-    };;
+    // const deleteKarmaPost = async (userData, karmaPosts) => {
+    //     try {
+    //         console.log(userData)
+    //         await deletePost({
+    //             variables: { karmaPostId: karmaPosts._id, },
+    //         });
+    //         const refundedUserKarma = userData.karma + karmaPosts.postValue;
+    //         await refundKarma({
+    //             variables: {
+    //                 username: userData.username,
+    //                 karma: refundedUserKarma,
+    //             }
+    //         })
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };;
 
     // Returns null if userdata is not present
     if (!userData) {
@@ -49,7 +49,7 @@ export default function ProfileActivityCard() {
             You created the job "{karmaPosts.postTitle}" at the location: {karmaPosts.address} , offering {karmaPosts.postValue} karma points. {karmaPosts.karmaHelpers.map(karmaHelpers => (
                 karmaHelpers.helperUsername + " " + "has signed up to help."
             ))}
-            <DeleteImage src="./assets/images/delete.png" onClick={() => deleteKarmaPost(userData, karmaPosts)} />
+            {/* <DeleteImage src="./assets/images/delete.png" onClick={() => deleteKarmaPost(userData, karmaPosts)} /> */}
         </CardContainer>))
     )
 }
